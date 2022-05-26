e25437d595e5aec023f30ec298208d6f9b57952f
Aashish KC <kcaashish@gmail.com>
2022-05-26 14:19:52 +0545

    add .env for database access

--- cmd/gotodo/main.go
+++ cmd/gotodo/main.go
@@ -4,19 +4,30 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/kcaashish/gotodo/postgres"
 	"github.com/kcaashish/gotodo/web"
 )
 
 func main() {
-	store, err := postgres.NewStore("postgres://postgres:secret@localhost/postgres?sslmode=disable")
+	if er := godotenv.Load(".env"); er != nil {
+		log.Fatal("Error loading .env file.")
+	}
+	port := os.Getenv("BACKEND_PORT")
+	user := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASSWORD")
+	host := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+	store, err := postgres.NewStore(fmt.Sprintf("postgresql://%s:%s@%s:%s/%s", user, dbPass, host, dbPort, dbName))
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	router := web.NewServer(store)
-	fmt.Println("Starting the server on: 3000...")
-	log.Fatal(http.ListenAndServe(":3000", router))
+	fmt.Printf("Starting the server on: %v...\n", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), router))
 }
--- go.mod
+++ go.mod
@@ -2,8 +2,9 @@ module github.com/kcaashish/gotodo
 
 go 1.18
 
-require github.com/google/uuid v1.3.0
-
-require github.com/jmoiron/sqlx v1.3.5
-
-require github.com/lib/pq v1.10.6
+require (
+        github.com/google/uuid v1.3.0
+        github.com/jmoiron/sqlx v1.3.5
+        github.com/lib/pq v1.10.6
+        github.com/joho/godotenv v1.4.0
+)
