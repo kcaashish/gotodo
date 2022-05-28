f7a6367629a9ea48ef7f7417e00e15a745e0a4d0
Aashish KC <kcaashish@gmail.com>
2022-05-28 14:54:01 +0545

    update schema for storing passwords and dates

--- migrations/1_create_tables.up.sql
+++ migrations/1_create_tables.up.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS users (
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     email VARCHAR(300) NOT NULL,
-    password VARCHAR(50) NOT NULL
+    password CHAR(60) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS todo_list (
@@ -12,20 +12,20 @@ CREATE TABLE IF NOT EXISTS todo_list (
     user_id UUID NOT NULL,
     title TEXT NOT NULL,
     description TEXT NOT NULL,
-    created_date TIMESTAMPTZ,
-    updated_date TIMESTAMPTZ,
-    due_date TIMESTAMPTZ NOT NULL,
+    created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_date TIMESTAMP,
+    due_date TIMESTAMP NOT NULL,
     completed BOOLEAN,
-    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS todo_entry (
     id UUID PRIMARY KEY,
     todolist_id UUID NOT NULL,
     content TEXT NOT NULL,
-    created_date TIMESTAMPTZ,
-    updated_date TIMESTAMPTZ,
-    due_date TIMESTAMPTZ NOT NULL,
+    created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_date TIMESTAMP,
+    due_date TIMESTAMP NOT NULL,
     completed BOOLEAN,
-    FOREIGN KEY (todolist_id) REFERENCES todo_list (id) ON DELETE CASCADE
+    FOREIGN KEY (todolist_id) REFERENCES todo_list (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
