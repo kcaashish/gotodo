5d47d107513e8a0ab28e981823edb8f7ce745992
Aashish KC <kcaashish@gmail.com>
2022-06-04 23:44:01 +0545

    added a method for refreshing token

--- web/middleware.go
+++ web/middleware.go
@@ -2,8 +2,11 @@ package web
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/golang-jwt/jwt"
 	"github.com/kcaashish/gotodo"
@@ -39,10 +42,53 @@ func Authenticate(next http.Handler) http.Handler {
 
 		if !token.Valid {
 			w.WriteHeader(http.StatusUnauthorized)
+			return
 		}
 
 		ctx := context.WithValue(r.Context(), "user", tk.UserID)
 		r.WithContext(ctx)
 		next.ServeHTTP(w, r)
+	})
+}
+
+func Refresh() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		tokenString := r.Header.Get("Authorization")
+
+		tk := &gotodo.Token{}
+
+		token, er := jwt.ParseWithClaims(tokenString, tk, func(t *jwt.Token) (interface{}, error) {
+			return []byte(os.Getenv("TOKEN_PASSWORD")), nil
+		})
+
+		if er != nil {
+			http.Error(w, er.Error(), http.StatusUnauthorized)
+			return
+		}
+
+		if !token.Valid {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		if time.Unix(tk.ExpiresAt, 0).Sub(time.Now()) > 30*time.Second {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		expiresAt := time.Now().Add(time.Minute * 5)
+
+		tk.ExpiresAt = expiresAt.Unix()
+
+		tokenRef := jwt.NewWithClaims(jwt.SigningMethodHS256, tk)
+
+		tokenString, errtk := tokenRef.SignedString([]byte(os.Getenv("TOKEN_PASSWORD")))
+		if errtk != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Println(errtk)
+		}
+
+		var resp = map[string]string{"refresh_token": string(tokenString)}
+		json.NewEncoder(w).Encode(resp)
 	})
 }
