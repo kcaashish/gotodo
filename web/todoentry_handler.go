b32f8b648df66dd193b90c5bd76d9cd50f056356
Aashish KC <kcaashish@gmail.com>
2022-06-12 06:00:31 +0545

    check if the te is inside the tl in all operations

--- web/todoentry_handler.go
+++ web/todoentry_handler.go
@@ -12,12 +12,18 @@ import (
 
 func (s *Server) getTodoEntry() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		id, _ := uuid.Parse(getField(r, 0))
+		tl_id, _ := uuid.Parse(getField(r, 0))
+		id, _ := uuid.Parse(getField(r, 1))
 
 		te, err := s.store.TodoEntry(id)
 		// er if the given id is not in the database
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		if te.TodoListID != tl_id {
+			http.Error(w, "Invalid request", http.StatusForbidden)
 			return
 		}
 
@@ -28,21 +34,32 @@ func (s *Server) getTodoEntry() http.HandlerFunc {
 
 func (s *Server) getTodoEntries() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		tl_id, _ := uuid.Parse(getField(r, 0))
 		tee, err := s.store.TodoEntriesByList()
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
+		teToReturn := make([]gotodo.TodoEntry, 0)
+		for _, te := range tee {
+			if te.TodoListID == tl_id {
+				teToReturn = append(teToReturn, te)
+			}
+		}
+
 		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(tee)
+		json.NewEncoder(w).Encode(teToReturn)
 	}
 }
 
 func (s *Server) createTodoEntry() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		tl_id, _ := uuid.Parse(getField(r, 0))
+
 		te := &gotodo.TodoEntry{}
 		te.ID = uuid.New()
+		te.TodoListID = tl_id
 
 		if err := json.NewDecoder(r.Body).Decode(te); err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
@@ -61,7 +78,23 @@ func (s *Server) createTodoEntry() http.HandlerFunc {
 
 func (s *Server) updateTodoEntry() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		id, _ := uuid.Parse(getField(r, 0))
+		tl_id, _ := uuid.Parse(getField(r, 0))
+		id, _ := uuid.Parse(getField(r, 1))
+
+		// first fetch te based on id
+		te_check, err := s.store.TodoEntry(id)
+		// er if the given id is not in the database
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		if te_check.TodoListID != tl_id {
+			http.Error(w, "Invalid request", http.StatusForbidden)
+			return
+		}
+
+		// if valid, proceed with update
 		te := &gotodo.TodoEntry{}
 
 		te.UpdatedAt = sql.NullTime{
@@ -86,13 +119,28 @@ func (s *Server) updateTodoEntry() http.HandlerFunc {
 
 func (s *Server) deleteTodoEntry() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		id, _ := uuid.Parse(getField(r, 0))
+		tl_id, _ := uuid.Parse(getField(r, 0))
+		id, _ := uuid.Parse(getField(r, 1))
+
+		// first fetch te based on id
+		te_check, err := s.store.TodoEntry(id)
+		// er if the given id is not in the database
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		if te_check.TodoListID != tl_id {
+			http.Error(w, "Invalid request", http.StatusForbidden)
+			return
+		}
 
 		if er := s.store.DeleteTodoEntry(id); er != nil {
 			http.Error(w, er.Error(), http.StatusBadRequest)
 			return
 		}
 
-		w.WriteHeader(http.StatusNoContent)
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(te_check)
 	}
 }
