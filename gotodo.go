efce93ca6efdf62b1024b5a55989c5658d4404fb
Aashish KC <kcaashish@gmail.com>
2022-06-12 06:11:42 +0545

    add createdat to user response

--- gotodo.go
+++ gotodo.go
@@ -25,6 +25,7 @@ type UserResponse struct {
 	FirstName         string    `json:"first_name"`
 	LastName          string    `json:"last_name"`
 	Email             string    `json:"email"`
+	CreatedAt         time.Time `json:"created_at"`
 	PasswordChangedAt time.Time `json:"password_changed_at"`
 }
 
